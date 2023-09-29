d2b57c9cb0d9c69a52700f3b4b62148f451fb5ad
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-09-29 13:49:47 +0330

    Add theme

---
+++ lib/common/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff80608B);
+const secondryColor = Color(0xffFF8A5B);
+const textColor = Color(0xff2c2130);
+const secondryTextColor = Color(0xffFFE2D6);
